4e9c9e98d7e7b65ee3c2cfe71e858438afb6c86c
Christopher Beech <beechchristopher350@gmail.com>
2021-12-15 15:31:17 -0700

    new portfolio updates

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const master = document.querySelector('.master');
-window.addEventListener('scroll', () => {
-    if(window.scrollY > master.offsetHeight - 2500){
-        master.classList.add('active');
-    }
-});
+// const master = document.querySelector('.master');
+// window.addEventListener('scroll', () => {
+//     if(window.scrollY > master.offsetHeight - 3700){
+//         master.classList.add('active');
+//     }
+// });
+
+
 
 // function bodyAppear(){
 //     if(window.scrollY > master.offsetHeight - 1500){
 //         master.classList.add('active');
 //     }
 // }
-
